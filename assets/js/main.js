706e68e8c4416d3a7bd2334aec91ee900ca7e0c2
Meng Fan Wang <mengfanwang724@gmail.com>
2021-09-20 01:26:45 -0400

    draft #2

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,48 +1,39 @@
 /* javascript */
 document.querySelector("form").addEventListener("submit", function(event) {
   var nameValue = document.getElementById("parkingNumber").value;
+  event.preventDefault();
+  var hintOn = false;
   if(nameValue == "87"){
     alert("Congratulations! You are a genius!");
     carMove();
   }
   else{
     alert("Sorry! Try again!");
-    document.getElementById("hint").innerHTML += `Would you like a hint?<br> <button onclick="clueAppear()" class="button">Hint</button>`;
-    myFunction();
+    if(!hintOn){
+      document.getElementById("hint").innerHTML = `Would you like a hint?<br>
+      <button onclick="clueAppear()" class="button">Hint</button>`;
+      myFunction();
+      hintOn = true;
+    }
   }
-  event.preventDefault();
 }, false);
 
-var id = null;
-function myMove() {
-  var elem = document.getElementById("car");
-  var pos = 0;
-  clearInterval(id);
-  id = setInterval(frame, 10);
-  function frame() {
-      pos++;
-      elem.style.top = pos + 'px';
-      elem.style.left = pos + 'px';
-  }
-}
-// function hintAppear(){
-//
-// }
+
 function clueAppear() {
-  document.getElementById("clue").innerHTML +=`Try looking at it from another perspective.`;
+  document.getElementById("clue").innerHTML =`Try looking at it from another perspective.`;
 }
 
 var id = null;
 function carMove() {
   var elem = document.getElementById("redCar");
-  var pos = 158;
+  var pos = 358;
   clearInterval(id);
   id = setInterval(frame, 10);
   function frame() {
-    if (pos == 350) {
+    if (pos == 550) {
       clearInterval(id);
     } else {
-      pos++;
+      pos+=2;
       elem.style.top = pos + 'px';
     }
   }
